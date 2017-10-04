bbb1773cd42d0645beaddabbc0e833a9a7eff055
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-10-04 08:52:36 -0500

    Update

---
+++ Chapter 8/src/exercises/SixArray.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class SixArray {
+
+	public static void main(String[] args) {
+/*		int randomNumbersMrBrosiusGaveMe [] = {23, 6, 47, 35, 2, 14};
+		int total;
+		int count;
+		System.out.print("The scores entered were: ");
+
+	for(int x = 0; x <= randomNumbersMrBrosiusGaveMe.length; ++x)
+		total += randomNumbersMrBrosiusGaveMe[count];
+		++count;
+		System.out.println(randomNumbersMrBrosiusGaveMe[x] + " ");
+	if (count != 0)
+		System.out.println("\n The average is " + (total * 1)/count); 
+	else
+		System.out.println("No scores entered");
+		*/
+	}
+
+}
